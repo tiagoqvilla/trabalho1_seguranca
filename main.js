f257137e4d76abdabb44a309caaa1e5e9da416ee
Tiago Villa <tiago.villa@edu.pucrs.br>
2023-09-19 21:30:18 -0300

    Adiciona função de adivinhação de chave

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ const {
   findKey,
   splitIntoBlocks,
 } = require('./vigenereUtils.js')
+
 const ALPHABET = [...'abcdefghijklmnopqrstuvwxyz']
+const MOST_FREQUENT_LETTER_PORTUGUESE = 'a'
+const MOST_FREQUENT_LETTER_ENGLISH = 'e'
+
 
 const textPortuguese = readFile('plaintext_portuguese.txt')
 const textEnglish = readFile('plaintext_english.txt')
@@ -20,39 +24,43 @@ let shifted = shifText(splittedText)
 let shiftedCounter = letterFrequencyCounter(shifted)
 let ioc = calculateIndexOfCoincidence(shiftedCounter, shifted.length)
 
-let blocks = splitIntoBlocks(textPortuguese, 7)
-console.log(blocks)
+let blocks = splitIntoBlocks(textEnglish, 7)
 let counts = blocks.map((block) => letterFrequencyCounter(block))
-console.log(counts)
 let totalsCount = textPortuguese.length
 
-let digitToFrequencies = []
-counts.forEach((count) => {
-  let totalsCount = Object.keys(count).reduce(
-    (acc, currentValue) => acc + count[currentValue],
-    0
-  )
+const createGroups = (blocks, keyLength) => {
+    let groups = {}
+    for (let i = 0; i < keyLength; i++) {
+        groups[`group_${i}`] = ''
+    }
 
-  let frequencies = []
-  Object.keys(count).forEach((entry) => {
-    let frequency = count[entry] / totalsCount
-    let characterFrequency = {}
-    characterFrequency[`${entry}`] = frequency
-    frequencies.push(characterFrequency)
-  })
-  digitToFrequencies.push(frequencies)
-})
+    blocks.forEach((block) => {
+        for (let i = 0; i < block.length; i++) {
+            groups[`group_${i}`] += block[i]
+          }
+    })
 
-const guessKey = (frequencies) => {
-  let possibleKey = ''
-  frequencies.forEach((frequency) => {
-    let mostFrequentChar = Object.keys(frequency[0])[0]
-    let keyInt = ALPHABET.indexOf(mostFrequentChar) - ALPHABET.indexOf('a')
-    keyInt = keyInt >= 0 ? keyInt : keyInt + ALPHABET.length
-    let key = ALPHABET[ALPHABET.indexOf('a') + keyInt]
-    possibleKey += key
-  })
+    return groups
 }
 
-guessKey(digitToFrequencies)
-decrypt(textEnglish, 'meunome')
+const getMaxValueLetter = (objectCount) => {
+    return Object.keys(objectCount).reduce((a, b) => objectCount[a] > objectCount[b] ? a : b)
+}
+
+const guessKey = (groups) => {
+    let probableKey = ''
+    Object.keys(groups).forEach((group) => {
+        let groupCount = letterFrequencyCounter(groups[group])
+        let highestLetterCount = getMaxValueLetter(groupCount)
+        let difference = ALPHABET.indexOf(highestLetterCount) - ALPHABET.indexOf(MOST_FREQUENT_LETTER_ENGLISH)
+        probableKey += ALPHABET.at(difference)
+    })
+    return probableKey
+}
+
+let groups = createGroups(blocks, 7)
+
+
+
+
+
